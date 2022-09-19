3ddf3f9af40943673d0e3ae1ad26423aa30126b7
M Youhan Jaigirdar <112583496+MYJBing@users.noreply.github.com>
2022-09-19 17:20:46 +0000

    I followed all the lab ch02 instructions

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -6,5 +6,16 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classes_per_week = 3
+cost_per_class= (cost_per_week/classes_per_week)
+print("cost_per_class:", cost_per_class)
 
 #Part B
+import random
+ 
+list = ("CPU", 7, 3.5, 2 , 5, "A")
+
+technology= random.choice(list)
+
+
+print("technology", technology)
