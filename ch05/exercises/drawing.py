71c7a9329fa2c7bb7484a4163361a6fdc63167f5
M Youhan Jaigirdar <112583496+MYJBing@users.noreply.github.com>
2022-10-12 21:39:16 +0000

    update

--- ch05/exercises/drawing.py
+++ ch05/exercises/drawing.py
@@ -1,4 +1,20 @@
-import turtle
-import random
 
-window.bgcolor()
+#define a function
+def star_pyramid(rows): 
+print(rows,id(rows)
+
+  
+
+
+
+#define a function
+def rstar_pyramid
+for i in range (rows, 0,-2)
+print( )
+
+levels = int(input("Enter Your Desired Pyramid Height"))
+
+starpyramid(levels)
+rstar_pyramid(levels)
+
+print(levels,)
