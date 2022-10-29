c616a558ab60791294947b0459f917d59a6ca402
M Youhan Jaigirdar <112583496+MYJBing@users.noreply.github.com>
2022-10-29 16:15:37 +0000

    Milestone1

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Preferred Name: Youhan
 Discord username: M Youhan Jaigirdar
 
 REPL url: [https://replit.com/@YouhanJaigirdar/portfolio-](https://replit.com/join/iufmyorfye-youhanjaigirdar)
+
--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,9 +1,22 @@
 import pygame
 import math
-import time
+
+ #PART A
+
+n = 101
+count = 0
+#print("Start: 101")
+while n!=1:
+  if n%2==0:
+    n/=2
+  else:
+    n=(3*n)+1
+  count+=1
+ print(n)
+  
+print("Count:", count)
+
+          
 
 
-pygame.init()
-display= pygame.display.set_mode
-i = int(input("introduce number : "))
-print(int(i))
+          
