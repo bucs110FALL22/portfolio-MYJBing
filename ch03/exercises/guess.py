0c4911b3483d40e95602f4e7231f377f940752a7
M Youhan Jaigirdar <112583496+MYJBing@users.noreply.github.com>
2022-10-29 18:37:53 +0000

    Chapter 5 lab fully done

---
+++ ch03/exercises/guess.py
@@ -0,0 +1,25 @@
+import random
+
+#generating random number
+number = random.randrange(1,11)
+
+
+chance = 0
+correct_guess = False
+
+for i in range(3):
+  
+  guess = input("Guess the number: ")
+  guess = int(guess)
+  chance += 1
+  if guess < number:
+    print("Too Low.")
+  elif guess > number:
+    print("Too High.")
+  else:  #(guess = number)
+    print("Correct!")
+    print("It took you", chance, "chances to get it correct")
+    
+    
+    
+    
