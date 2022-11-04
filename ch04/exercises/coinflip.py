caf94a32eb9c8ec9284f13dc29786bfd5aa4a36d
M Youhan Jaigirdar <112583496+MYJBing@users.noreply.github.com>
2022-11-04 16:05:39 +0000

    coinflip

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -17,12 +17,11 @@ joy.goto(x, y)
 coin= ['heads','tails']
 
 is_in_window= True
+x_range = window.window_width()/2
+y_range = window.window_height()/2
 
 while is_in_window:
   result = random.choice(coin)
-
-  x_range = window.window_width()/2
-  y_range = window.window_height()/2
 
   if (result == 'heads'):
     print(result)
@@ -30,7 +29,7 @@ while is_in_window:
     joy.forward(50)
     x = joy.xcor()
     y = joy.ycor()
-    print("(x,y)")
+    print(f"({x},{y})")
 
   elif (result == 'tails'):
     print(result)
@@ -38,14 +37,15 @@ while is_in_window:
     joy.forward(50)
     x = joy.xcor()
     y = joy.ycor()
-    print("(x,y)")
+    print(f"({x},{y})")
 
 
-if abs(x) > 150 or abs(y) > 150:
-   is_in_window = False
+    if abs(x) > 150 or abs(y) > 150:
+       is_in_window = False
 
 window.exitonclick()
 
 #Done
+#Up and working
     
-
+window.exitonclick
