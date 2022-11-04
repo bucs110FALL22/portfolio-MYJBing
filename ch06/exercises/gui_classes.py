9d65478fedd96f8819244b7487e46e484f675b2d
M Youhan Jaigirdar <112583496+MYJBing@users.noreply.github.com>
2022-11-04 15:43:26 +0000

    Chapter 6 exercise

--- ch06/exercises/gui_classes.py
+++ ch06/exercises/gui_classes.py
@@ -1,4 +1,27 @@
 import gui_classes
 
+class Player:
+	  def __init__(self):
+		# “””
+		# 	Initialize the player object
+               
+  #              args: pnum [int] the player's id number  
+		# “””
+          self.player_num = 1
+          self.lives = 3 # players always start with 3 lives
+          self.is_large = False # player always starts small
+
+class character():
+  def __init__(self, is_moving, size, height):
+    self.is_Moving = True
+    self.size = True
+    self.height = 15
+
+class background():
+  def __init__(self, cloud, coin):
+    self.cloud = 4
+    self.coin = 22
+
+    
 
 
