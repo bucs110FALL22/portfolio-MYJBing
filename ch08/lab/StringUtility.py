f280b842a5a26873dd6abca3e2ce3bc1e976a40f
M Youhan Jaigirdar <112583496+MYJBing@users.noreply.github.com>
2022-12-14 18:22:10 +0000

    c

--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -7,38 +7,68 @@ class StringUtility:
     return self.string
 
 
-def vowels(self):
-    count = 0
-    for i in self.string:
-      if(i=="a" or i=="e"or i=="i" or i=="o" or i=="u"):
-        count = count + 1
-      if (count >5):
-        return "many"
-      else:
-        return count
+  def __str__(self):
+        """Returns the string itself, unchanged."""
+        return self.string
 
-def bothEnds(self):
-    size = len(self.string)
-    if (size <= 2):
-      return ""
-    else:
-      stng = self.string[0] + self.string[1] + self.string[-2] + self.string[-1]
-      return stng
-
-def fixStart(self):
-    if len(self.string)>=1:
-      firstchar= self.string[0]
+  def vowels(self):
+      """Counts the number of vowels in the string"""
+      count = 0
       for i in self.string:
-        s_star = self.string[1:].replace(firstchar,"*")
-        return self.string[0]+ str
-    else:
-      return""
+        if(i=="a" or i=="e"or i=="i" or i=="o" or i=="u"):
+          count = count + 1
+        if (count >5):
+          return "many"
+        else:
+          return count
+  
+  def bothEnds(self):
+      """Returns a string made of the first 2 and last 2 chars of the original string"""
+    
+      size = len(self.string)
+      if (size <= 2):
+        return ""
+      else:
+        stng = self.string[0] + self.string[1] + self.string[-2] + self.string[-1]
+        return stng
+  
+  def fixStart(self):
+      """Returns a string where the first character have been changed to '*'"""
+      if len(self.string)>=1:
+        first_char= self.string[0]
+        for i in self.string:
+          s_star = self.string[1:].replace(firstchar,"*")
+          return self.string[0]+ str
+      else:
+        return""
+  
+  def asciiSum(self):
+      """Returns the sum of the ASCII values of everything in the string."""
+  
+      str = 0
+      for i in range(len(self.string)):
+          str = str + ord(self.string[i])
+      return str
+    
+  #ord function returns the character's equivalent integer value.
+  
+  def cipher(self):
+        """Returns an encoded string. """
+        
+        upper = 65
+        lower = 97
+        alpha = 26
+        list = ""
+  
+        for j in self.string:
+          if j == " ":
+            list = list+j
+          elif j.isupper():
+            list = list+chr((ord(j)+len(self.string)-upper)%alpha+upper)
+          else:
+            list = list+chr((ord(j)+len(self.string)-lower)%alpha+lower)
+        return list
 
-def asciiSum(self):
-    str = 0
-    for i in range(len(self.string)):
-        str = str + ord(self.string[i])
-    return str
 
 
 
